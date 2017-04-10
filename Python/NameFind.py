75cbbf6f1b6cc1d0a70142b01cd914a2afe9b867
Lahiru Dinalankara <dinalankara.dinalankara@postgrad.plymouth.ac.uk>
2017-04-10 21:37:16 +0100

    Updated NameFind.py

--- Python/NameFind.py
+++ Python/NameFind.py
@@ -34,3 +34,25 @@ def AddName():
     print ("Name Stored in " + str(ID))
     Info.close()
     return ID
+
+#       ------------------- DRAW THE BOX AROUND THE FACE, ID and CONFIDENCE---
+def DispID(x, y, w, h, NAME, Image):
+
+    cv2.rectangle(gray, (x, y), (x + w, y + h), (255, 255, 255), 1)     #   Draw a rectangle arround the face
+
+     #  ------------------------------------    THE POSITION OF THE ID BOX           
+
+    Name_y_pos = y - 10
+    Name_X_pos = x + w/2 - (len(NAME)*7/2)
+
+    if Name_X_pos < 0:
+        Name_X_pos = 0
+    elif (Name_X_pos +10 + (len(NAME) * 7) > gray.shape[1]):
+          Name_X_pos= Name_X_pos - (Name_X_pos +10 + (len(NAME) * 7) - (gray.shape[1]))
+    if Name_y_pos < 0:
+        Name_y_pos = Name_y_pos = y + h + 10
+          
+ #  ------------------------------------    THE DRAWING OF THE BOX AND ID   
+    cv2.rectangle(Image, (Name_X_pos-10, Name_y_pos-25), (Name_X_pos +10 + (len(NAME) * 7), Name_y_pos-1), (0,0,0), -2)           #   Draw a Black Rectangle over the face frame
+    cv2.rectangle(Image, (Name_X_pos-10, Name_y_pos-25), (Name_X_pos +10 + (len(NAME) * 7), Name_y_pos-1), (255, 255, 255), 1) 
+    cv2.putText(Image, NAME, (Name_X_pos, Name_y_pos - 10), cv2.FONT_HERSHEY_DUPLEX, .4, (255, 255, 255))    #   Print the name of the ID
